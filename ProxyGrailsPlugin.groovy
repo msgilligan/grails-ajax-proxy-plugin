2dae4faddd0e7e27a22ca14115e8cf140033dcb5
Burt Beckwith <beckwithb@vmware.com>
2012-01-06 09:50:04 -0500

    cleanup

--- ProxyGrailsPlugin.groovy
+++ ProxyGrailsPlugin.groovy
@@ -1,27 +1,17 @@
-import grails.util.GrailsUtil
 import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class ProxyGrailsPlugin {
-    // the plugin version
     def version = "0.1.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
 
-    // TODO Fill in these fields
     def author = "Sean Gilligan"
     def authorEmail = "sean at msgilligan dot com"
-    def title = "Ajax Proxy Plugin (for cross-domain requests)"
-    def description = '''\\
-Brief description of the plugin.
-'''
+    def title = "Ajax Proxy Plugin"
+    def description = 'Ajax Proxy Plugin (for cross-domain requests)'
 
-    // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/proxy"
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
       def config = ConfigurationHolder.config.plugins.proxy
 
       def proxySchemeConfig = config.proxyScheme
@@ -72,30 +62,6 @@ Brief description of the plugin.
           'servlet-name'('ProxyServlet')
           'url-pattern'('/proxy/*')
         }
-
       }
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 }
